afee7497cc85d914d7129ba2b4abde04c3917496
tongjingwei <tongjingwei@shining3d.com>
2025-06-10 20:40:29 +0800

    change init info

--- init.rb
+++ init.rb
@@ -1,9 +1,11 @@
 require File.expand_path 'lib/redmine_subject_autocomplete/hooks', __dir__
 
-Redmine::Plugin.register :redmine_subject_autocomplete do
-  name "Redmine Subject Autocomplete"
-  author "Intera GmbH"
-  author_url "https://github.com/intera"
-  version "1.0.0"
-  description "Adds autocompletion to the new issue subject field to prevent duplicate ticket creation"
+Redmine::Plugin.register :redmine_subject_autocomplete_chinese do
+  name "Redmine Subject Autocomplete for Chinese"
+  author "jjustwin"
+  author_url "https://github.com/jjustwin"
+  version "1.0.1"
+  description "Adds autocompletion to the new issue subject field to prevent duplicate ticket creation, modify by jjustwin for Chinese support."
+
+
 end
