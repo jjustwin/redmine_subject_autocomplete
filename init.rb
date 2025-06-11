d80bf453a0b0d5c026040b752f320a912beb643b
tongjingwei <tongjingwei@shining3d.com>
2025-06-11 18:44:43 +0800

    修改jieba名称

--- init.rb
+++ init.rb
@@ -3,10 +3,10 @@ require File.expand_path 'lib/redmine_subject_autocomplete/hooks', __dir__
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __FILE__)
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
 
-Redmine::Plugin.register :redmine_subject_autocomplete_chinese do
-  name "Redmine Subject Autocomplete for Chinese"
+Redmine::Plugin.register :redmine_subject_autocomplete_jieba do
+  name "Redmine Subject Autocomplete for Chinese use jieba"
   author "jjustwin"
-  author_url ""
+  author_url "https://github.com/jjustwin/"
   version "1.0.1"
   description "Adds autocompletion to the new issue subject field to prevent duplicate ticket creation, modify by jjustwin for Chinese support."
 
